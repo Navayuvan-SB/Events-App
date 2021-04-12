109a81c6a7e60973a4f8204630e8c228b37a1959
navayuvansb <sb.navayuvan@gmail.com>
2021-04-12 15:51:42 +0530

    Change view to return only the events the user own

--- events/views.py
+++ events/views.py
@@ -1,10 +1,14 @@
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Event
 
 
-class EventsListView(generic.ListView):
+class EventsListView(LoginRequiredMixin, generic.ListView):
     model = Event
 
+    def get_queryset(self):
+        return Event.objects.filter(created_by__exact=self.request.user)
 
-class EventDetailView(generic.DetailView):
+
+class EventDetailView(LoginRequiredMixin, generic.DetailView):
     model = Event

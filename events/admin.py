f5f0e049eaa4e75471f177cdd7ad1b8fb1408122
navayuvansb <sb.navayuvan@gmail.com>
2021-04-12 11:46:36 +0530

    Register Event models in admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Event, EventTime
 
-# Register your models here.
+
+class EventTimeInlineAdmin(admin.TabularInline):
+    model = EventTime
+    fields = ['event_date', 'from_time', 'end_time', 'all_day']
+    extra = 2
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by')
+    inlines = [EventTimeInlineAdmin]

e726febbe122729b9d503ac717aa80254478b3a8
navayuvansb <sb.navayuvan@gmail.com>
2021-04-12 11:52:24 +0530

    Add URL route for default user authentication

--- eventsapp/urls.py
+++ eventsapp/urls.py
@@ -22,5 +22,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

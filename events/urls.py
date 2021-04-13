582fdbb486f3abd1168602d9369d0b8c17713275
navayuvansb <sb.navayuvan@gmail.com>
2021-04-13 13:55:56 +0530

    Define routes for the following
    Routes:
    Event
      - Create
      - Update
      - Delete
    Event Time
      - Create
      - Update
      - Delete

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.EventsListView.as_view(), name='events'),
-    path('<int:pk>', views.EventDetailView.as_view(), name='event-detail')
+    path('new/', views.EventCreateView.as_view(), name='add-event'),
+    path('<int:pk>/edit', views.EventEditView.as_view(), name='edit-event'),
+    path('<int:pk>/delete', views.EventDeleteView.as_view(), name='delete-event'),
+    path('<int:pk>/time/new/', views.EventTimeCreateView.as_view(),
+         name='add-event-timings'),
+    path('<int:eventid>/time/edit/<int:pk>', views.EventTimeEditView.as_view(),
+         name='edit-event-timings'),
+    path('<int:eventid>/time/delete/<int:pk>', views.EventTimeDeleteView.as_view(),
+         name='delete-event-timings'),
+    path('<int:pk>', views.EventDetailView.as_view(), name='event-detail'),
 ]
